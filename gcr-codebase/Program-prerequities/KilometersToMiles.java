4b78027b5bc6e77f15233b41e037b7502d3d4886
KumariKunduShukla <kundushukla55@gmail.com>
2025-12-17 00:25:07 +0530

    [Kumari] added : converted kilometers to miles

---
+++ gcr-codebase/Program-prerequities/KilometersToMiles.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class KilometersToMiles
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+        System.out.print("Enter distance in kilometers: ");
+        double kilometers = sc.nextDouble();
+		 double miles=kilometers * 0.621371;
+		 System.out.println("Converted to miles=" +miles);
+		 }
+		 }
