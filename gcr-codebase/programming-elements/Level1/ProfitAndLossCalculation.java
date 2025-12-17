42d947b07fca180ca778b110386a9b5c370185ee
KumariKunduShukla <kundushukla55@gmail.com>
2025-12-17 14:24:56 +0530

    [Kumari] added : profit and loss calculation

---
+++ gcr-codebase/programming-elements/Level1/ProfitAndLossCalculation.java
@@ -0,0 +1,20 @@
+// create profit and loss calculation Class
+public class ProfitAndLossCalculation
+{
+public static void main(String[] argz)
+{
+//declare and initialize variables
+ int costPrice=129;
+ int sellingPrice=191;
+	
+ double profit=sellingPrice-costPrice; //calculating profit
+ double profitPercentage=profit/costPrice*100; //calculating profit percentage
+	
+ //Display the result
+ System.out.println("The Cost Price is INR "+ costPrice+" and Selling Price is INR "+sellingPrice+
+			   " \nThe Profit is INR "+profit+" and the Profit Percentage is "+profitPercentage);    
+			   }
+			   }
+			   
+	
+	
