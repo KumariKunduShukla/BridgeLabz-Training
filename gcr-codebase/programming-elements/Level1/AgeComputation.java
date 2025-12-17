2c5b873f678527c4668ba53a3b3bd8accf5f4bec
KumariKunduShukla <kundushukla55@gmail.com>
2025-12-17 12:03:51 +0530

    [Kumari] added : age computation

---
+++ gcr-codebase/programming-elements/Level1/AgeComputation.java
@@ -0,0 +1,19 @@
+
+// create AgeComputation Class to compute the current age
+public class AgeComputation
+{
+public static void main(String[] args)
+{
+
+// create a variable name to indicate the person age
+String name="Harry";
+// create a variable to indicate the birth year of person
+int birthYear=2000;
+// craete a variable to indicate the current year 
+int currentYear=2024;
+// create a variable age to calculate the age 
+int age=currentYear-birthYear;
+// printing the age of person
+System.out.println("Harry's age in "+currentYear+" is "+age );
+}
+}
