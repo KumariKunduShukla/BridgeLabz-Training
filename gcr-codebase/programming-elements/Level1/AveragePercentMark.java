c523b84719f3cc62d2ce0ffd6cce828428fce059
KumariKunduShukla <kundushukla55@gmail.com>
2025-12-17 12:23:07 +0530

    [Kumari] added : average percent mark

---
+++ gcr-codebase/programming-elements/Level1/AveragePercentMark.java
@@ -0,0 +1,19 @@
+// create AveragePercentMark Class
+class AveragePercentMark
+{
+public static void main(String[] args)
+{
+// marks obtained by Sam in each subject (out of 100)
+int mathsMark=94;
+int physicsMark=95;
+int chemistryMark=96;
+// calculate total marks
+int totalMark=mathsMark + physicsMark + chemistryMark;
+// Calculate average percentage
+double averageMark=totalMark/3.0;
+ // Display the result
+ System.out.println("Sam's average mark in PCM is " + averageMark);
+    }
+	}
+	
+
