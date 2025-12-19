940054d7bcc1aef015b1e412de96aa1e803d1347
KumariKunduShukla <kundushukla55@gmail.com>
2025-12-20 00:05:10 +0530

    [Kumari] : added Youngest and tallest among three friends logic

---
+++ gcr-codebase/arrays/Level2/YoungestAndTallest.java
@@ -0,0 +1,49 @@
+// Program to find the youngest and tallest among three friends
+import java.util.Scanner;
+
+public class YoungestAndTallest{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Names of friends
+        String[] names = {"Amar", "Akbar", "Anthony"};
+
+        // Arrays to store age and height
+        int[] age = new int[3];
+        double[] height = new double[3];
+
+        // Take input for age and height
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            age[i] = sc.nextInt();
+
+            System.out.print("Enter height of " + names[i] + " (in cm): ");
+            height[i] = sc.nextDouble();
+        }
+
+        // Assume first friend is youngest and tallest
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        // Find youngest and tallest
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < age[youngestIndex]) {
+                youngestIndex = i;
+            }
+            if (height[i] > height[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Display results
+        System.out.println("\nYoungest Friend: " + names[youngestIndex]);
+        System.out.println("Tallest Friend: " + names[tallestIndex]);
+        
+        sc.close();
+    }
+}
+
+
+
+
+
