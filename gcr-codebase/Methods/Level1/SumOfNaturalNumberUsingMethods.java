52abd960625a8eb6aa1e5bc03fea290c2ee63d6f
KumariKunduShukla <kundushukla55@gmail.com>
2025-12-20 15:21:04 +0530

    [Kumari] : included sum of natural numbers

---
+++ gcr-codebase/Methods/Level1/SumOfNaturalNumberUsingMethods.java
@@ -0,0 +1,31 @@
+// Program to find the sum of N narural numbers
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumberUsingMethods{
+    
+	// Creating method to calculate sum
+	public static int calculateSum(int number){
+		int sum=0;
+	for(int i=1;i<=number;i++){
+	 sum=sum+i;
+	}
+	return sum;
+	}
+	
+	// Main method
+	public static void main(String[] args){
+	// Create scanner object
+	Scanner sc=new Scanner(System.in);
+	
+	// Take user input
+	System.out.print("Enter the number: ");
+	int number=sc.nextInt();
+	
+	// Displaying sum using method
+	System.out.println(calculateSum(number));
+	
+	// Closing scanner stream
+	sc.close();
+	}
+   }
