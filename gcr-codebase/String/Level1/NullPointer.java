14704a8873c14114d854d0c8d1886b6480a0028d
KumariKunduShukla <kundushukla55@gmail.com>
2025-12-22 16:22:31 +0530

    [Kumari] : included Null pointer exception logic

---
+++ gcr-codebase/String/Level1/NullPointer.java
@@ -0,0 +1,33 @@
+// Program to demonstrate NullPointerException
+ 
+public class NullPointer{
+
+    // Method to generate NullPointerException
+    static void generateException() {
+        String text = null;        
+        System.out.println(text.length()); 
+    }
+
+    // Method to handle NullPointerException
+    static void handleException() {
+        String text = null; 
+        try {
+            System.out.println(text.length()); 
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException handled successfully");
+            System.out.println("Reason: Object reference is null");
+        }
+    }
+
+    //Main method
+    public static void main(String[] args) {
+
+        // generate exception
+        System.out.println("Generating Exception:");
+        generateException();
+
+        // handle the exception
+        System.out.println("\nHandling Exception:");
+        handleException();
+    }
+}
