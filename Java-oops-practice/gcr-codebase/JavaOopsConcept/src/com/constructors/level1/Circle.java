0c06a48527da5937cbe95023f8113ac303d3743e
KumariKunduShukla <kundushukla55@gmail.com>
2025-12-28 22:20:47 +0530

    [Kumari] : added circle class as well as its main method

---
+++ Java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructors/level1/Circle.java
@@ -0,0 +1,23 @@
+package com.constructors.level1;
+
+public class Circle {
+	// Attribute
+			private double radius;
+
+			// default constructor
+			public Circle() {
+				this(5.0);
+			}
+
+			// parameterized constructor
+			public Circle(double radius) {
+				this.radius = radius;
+			}
+
+			// display result
+			void display() {
+				System.out.println("Radius: " + this.radius);
+				System.out.println();
+			}
+	}
+
