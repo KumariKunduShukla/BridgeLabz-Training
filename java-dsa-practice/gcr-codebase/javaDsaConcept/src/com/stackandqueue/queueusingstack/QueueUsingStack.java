54471dc0ff6983135fa35540ababf9126354af22
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-07 21:31:28 +0530

    [Kumari]: implemented queue using stack

---
+++ java-dsa-practice/gcr-codebase/javaDsaConcept/src/com/stackandqueue/queueusingstack/QueueUsingStack.java
@@ -0,0 +1,39 @@
+package com.stackandqueue.queueusingstack;
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+	Stack<Integer> stackEnq = new Stack<>();
+    Stack<Integer> stackDeq = new Stack<>();
+
+    // Enqueue operation
+    void enqueue(int x) {
+        stackEnq.push(x);
+    }
+
+    // Dequeue operation
+    int dequeue() {
+        if (stackDeq.isEmpty()) {
+            while (!stackEnq.isEmpty()) {
+                stackDeq.push(stackEnq.pop());
+            }
+        }
+
+        if (stackDeq.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        return stackDeq.pop();
+    }
+
+    // Display front element
+    int peek() {
+        if (stackDeq.isEmpty()) {
+            while (!stackEnq.isEmpty()) {
+                stackDeq.push(stackEnq.pop());
+            }
+        }
+        return stackDeq.isEmpty() ? -1 : stackDeq.peek();
+    }
+}
---
+++ java-dsa-practice/gcr-codebase/javaDsaConcept/src/com/stackandqueue/queueusingstack/QueueUsingStacksDriver.java
@@ -0,0 +1,18 @@
+package com.stackandqueue.queueusingstack;
+
+public class QueueUsingStacksDriver {
+	public static void main(String[] args) {
+        QueueUsingStack q = new QueueUsingStack();
+
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+
+        System.out.println(q.dequeue()); // 10
+        System.out.println(q.dequeue()); // 20
+
+        q.enqueue(40);
+        System.out.println(q.dequeue()); // 30
+        System.out.println("Peek element:"+q.peek());    // 40
+    }
+}
