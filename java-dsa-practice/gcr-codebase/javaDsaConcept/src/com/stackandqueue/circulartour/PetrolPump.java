ab6e7521ae6d1e82c9fb3572143065df19d1114e
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-07 21:56:19 +0530

    [Kumari]: included circular tour problem using queue

---
+++ java-dsa-practice/gcr-codebase/javaDsaConcept/src/com/stackandqueue/circulartour/PetrolPump.java
@@ -0,0 +1,11 @@
+package com.stackandqueue.circulartour;
+
+public class PetrolPump {
+	int petrol;
+    int distance;
+
+    PetrolPump(int petrol, int distance) {
+        this.petrol = petrol;
+        this.distance = distance;
+    }
+}
