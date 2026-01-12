f061a19e6fa072a0bdce9f4969955c6a6cc56984
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-12 17:17:48 +0530

    [Kumari]: added Call center aap

---
+++ java-dsa-practice/scenario-based-codebase/scenarioDsaPractice/src/com/callcenter/CallCenterAap.java
@@ -0,0 +1,28 @@
+package com.callcenter;
+
+public class CallCenterAap {
+	public static void main(String[] args) {
+
+        CallCenter manager = new CallCenter();
+
+        Customer c1 = new Customer("Cust101", false);
+        Customer c2 = new Customer("Cust102", true);
+        Customer c3 = new Customer("Cust103", false);
+        Customer c4 = new Customer("Cust104", true);
+        Customer c5 = new Customer("Cust101", false);
+
+        manager.receiveCall(c1);
+        manager.receiveCall(c2);
+        manager.receiveCall(c3);
+        manager.receiveCall(c4);
+        manager.receiveCall(c5);
+
+        manager.handleNextCall();
+        manager.handleNextCall();
+        manager.handleNextCall();
+        manager.handleNextCall();
+        manager.handleNextCall(); // no calls waiting
+
+        manager.displayCallCounts();
+    }
+}
