4568782ed93eb8ce83384074036298198a7063df
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-12 13:17:29 +0530

    [Kumari]: modified Ambulance Route

--- java-dsa-practice/scenario-based-codebase/scenarioDsaPractice/src/com/ambulanceroute/AmbulanceRoute.java
+++ java-dsa-practice/scenario-based-codebase/scenarioDsaPractice/src/com/ambulanceroute/AmbulanceRoute.java
@@ -10,16 +10,12 @@ public class AmbulanceRoute {
         route.addUnit("Surgery", true);
         route.addUnit("ICU", false);
 
-        System.out.println(" Hospital Route:");
         route.displayRoute();
 
-        System.out.println("\n Searching for available unit...");
         route.findAvailableUnit();
 
-        System.out.println("\n Removing Radiology (Maintenance)");
         route.removeUnit("Radiology");
 
-        System.out.println("\n Updated Route:");
         route.displayRoute();
     }
 }
