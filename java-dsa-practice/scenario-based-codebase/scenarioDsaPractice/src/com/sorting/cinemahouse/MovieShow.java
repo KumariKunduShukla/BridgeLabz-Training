6c71ea15a7a2e7b9e629bebd8ad007372035317d
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-17 23:25:39 +0530

    [Kumari] : included Cinema House using bubble sort

---
+++ java-dsa-practice/scenario-based-codebase/scenarioDsaPractice/src/com/sorting/cinemahouse/MovieShow.java
@@ -0,0 +1,11 @@
+package com.sorting.cinemahouse;
+
+public class MovieShow {
+	String movieName;
+    int showTime;   // time in 24-hour format 
+    
+    MovieShow(String movieName, int showTime) {
+        this.movieName = movieName;
+        this.showTime = showTime;
+    }
+}
