9e394f8b4326aa1b1bfdb6ecbb351229ff72b1c8
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-18 16:08:48 +0530

    [Kumari] : included reverse list without using inbuilt function

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/list/ReverseArrayList.java
@@ -0,0 +1,26 @@
+package com.collections.list;
+
+import java.util.*;
+
+public class ReverseArrayList {
+	public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        int start = 0;
+        int end = list.size() - 1;
+
+        while (start < end) {
+            int temp = list.get(start);
+            list.set(start, list.get(end));
+            list.set(end, temp);
+
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println();
+        System.out.println("Reversed List:");
+        System.out.println(list);
+    }
+}
