24f1c77ac8679b5006d94666d8924099b0187ed3
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-19 16:17:18 +0530

    [Kumari] : included Flash Dealz using quick sort

---
+++ java-dsa-practice/scenario-based-codebase/scenarioDsaPractice/src/com/sorting/flashdealz/Product.java
@@ -0,0 +1,16 @@
+package com.sorting.flashdealz;
+
+class Product {
+	
+	//attributes
+    int id;
+    String name;
+    int discount;
+    
+    // parameterised constructor
+    Product(int id, String name, int discount) {
+        this.id = id;
+        this.name = name;
+        this.discount = discount;
+    }
+}
