a23eae66a24a23957307dbdf796f4eeb2d84e143
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-21 11:20:23 +0530

    [Kumari] : included Movie time using insertion sort

---
+++ java-dsa-practice/scenario-based-codebase/scenarioDsaPractice/src/com/movietime/Show.java
@@ -0,0 +1,12 @@
+package com.movietime;
+
+class Show {
+	// attributes
+    String movieName;
+    int time; // time in minutes (e.g., 18:30 → 1110)
+
+    Show(String movieName, int time) {
+        this.movieName = movieName;
+        this.time = time;
+    }
+}
