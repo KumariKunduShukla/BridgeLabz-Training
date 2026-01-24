24e92a78bd832f697081cd716592dde1f3669b9d
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-25 00:40:38 +0530

    [Kumari] : added email extract from given text using regex

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/regex/EmailExtract.java
@@ -0,0 +1,20 @@
+package com.regex;
+
+import java.util.regex.*;
+
+public class EmailExtract {
+	public static void main(String[] args) {
+
+        String text = "Contact us at support@example.com and info@company.org";
+
+        // Regex for email validation
+        String regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
