3ba55b3f567797b09094c119e16956fa0194f271
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-24 11:29:48 +0530

    [Kumari] : implemented exception-propagation

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exceptionhandling/ExceptionPropagation.java
@@ -0,0 +1,22 @@
+package com.exceptionhandling;
+
+public class ExceptionPropagation {
+	
+	static void method1() {
+        int x = 10 / 0;   // ArithmeticException
+    }
+
+    static void method2() {
+        method1();       // Exception propagates
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            method2();   // Caught here
+        } 
+        catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
