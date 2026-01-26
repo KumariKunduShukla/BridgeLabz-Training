8fa31c66bfd05968f856b0a043eafb7e2b602fcf
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-26 22:37:10 +0530

    [Kumari] : added accessing private field using reflection

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/ReflectionPrivateFieldExample.java
@@ -0,0 +1,30 @@
+package com.reflection;
+
+import java.lang.reflect.Field;
+
+// Class with private field
+class Person{
+    private int age;
+}
+
+// Main class to demonstrate reflection on private field
+public class ReflectionPrivateFieldExample{
+    public static void main(String[] args) throws Exception{
+
+        // Load class
+        Class<?> cls = Person.class;
+
+        // Create object
+        Person p = new Person();
+
+        // Access private field
+        Field field = cls.getDeclaredField("age");
+        field.setAccessible(true);   // allow access to private field
+
+        // Modify value
+        field.setInt(p, 34);
+
+        // Retrieve value
+        System.out.println("Age: " + field.getInt(p));
+    }
+}
