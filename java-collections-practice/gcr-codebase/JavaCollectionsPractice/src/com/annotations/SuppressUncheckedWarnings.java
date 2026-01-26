7fe791ca5259716184ac05c9661d3437c32da168
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-26 23:52:24 +0530

    [Kumari] : added use @SupressWarning annotation

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/SuppressUncheckedWarnings.java
@@ -0,0 +1,17 @@
+package com.annotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuppressUncheckedWarnings {
+    public static void main(String[] args) {
+
+        @SuppressWarnings("unchecked")
+        List list = new ArrayList();   // unchecked warning suppressed
+
+        list.add("Java");
+        list.add(100);
+
+        System.out.println(list);
+    }
+}
