262c87de0da72a7b899fa7ebd7a032a97cf56444
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-27 00:41:19 +0530

    [Kumari] : copied text in uppercase from from input file to output file in lowercase

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/streams/UpperToLowerCase.java
@@ -0,0 +1,31 @@
+package com.streams;
+
+import java.io.*;
+
+public class UpperToLowerCase {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        try {
+            br = new BufferedReader(new FileReader("src/input.txt"));
+            bw = new BufferedWriter(new FileWriter("src/output.txt"));
+            int ch;
+            while ((ch = br.read()) != -1) {
+                bw.write(Character.toLowerCase((char) ch));
+            }
+            System.out.println("Conversion completed successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) br.close();
+                if (bw != null) bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
