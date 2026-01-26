0191538476ab1ea78512dced05ef31250355acb2
KumariKunduShukla <kundushukla55@gmail.com>
2026-01-26 23:39:13 +0530

    [Kumari] : added displaying class information using reflection

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/Student.java
@@ -0,0 +1,17 @@
+package com.reflection;
+
+class Student {
+    private int id;
+    private String name;
+
+    public Student() {}
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println(id + " " + name);
+    }
+}
