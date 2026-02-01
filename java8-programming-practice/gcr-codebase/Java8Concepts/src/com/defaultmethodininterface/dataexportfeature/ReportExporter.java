92460f065ed09889fcd5676e9b157e9f09c75198
KumariKunduShukla <kundushukla55@gmail.com>
2026-02-01 22:05:48 +0530

    [Kumari] : modified data export feature using default method in interface

---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/defaultmethodininterface/dataexportfeature/ReportExporter.java
@@ -0,0 +1,14 @@
+package com.defaultmethodininterface.dataexportfeature;
+
+public interface ReportExporter {
+
+    void exportToCSV(String data);
+
+    void exportToPDF(String data);
+
+    // New feature added later (backward compatible)
+    default void exportToJSON(String data) {
+        System.out.println("Default JSON Export:");
+        System.out.println("{ \"report\": \"" + data + "\" }");
+    }
+}
