cb510da5a2df5c097a13ececa05bf88b0475f19b
KumariKunduShukla <kundushukla55@gmail.com>
2026-02-01 22:13:12 +0530

    [Kumari] : added smart vehicle dashboard using default method in interface

---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/defaultmethodininterface/smartvehicledashboard/ElectricCar.java
@@ -0,0 +1,14 @@
+package com.defaultmethodininterface.smartvehicledashboard;
+
+public class ElectricCar implements Vehicle {
+
+    @Override
+    public void displaySpeed() {
+        System.out.println("Electric Car Speed: 75 km/h");
+    }
+
+    @Override
+    public void displayBattery() {
+        System.out.println("Battery: 85%");
+    }
+}
